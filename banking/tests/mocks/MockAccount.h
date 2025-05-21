2802c0d825eab9c3b2cdd6168dd7afeeaa079bcb
ustalol <david.vut@mail.ru>
2025-05-21 17:34:34 +0300

    a

--- banking/tests/mocks/MockAccount.h
+++ banking/tests/mocks/MockAccount.h
@@ -7,7 +7,7 @@ public:
     MockAccount(int id, int balance) : Account(id, balance) {}
     
     MOCK_CONST_METHOD0(GetBalance, int());
-    MOCK_METHOD1(ChangeBalance, void(int));
+    MOCK_METHOD1(ChangeBalance, bool(int));  // Изменено на bool
     MOCK_METHOD0(Lock, void());
     MOCK_METHOD0(Unlock, void());
 };
